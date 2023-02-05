730e25f148ac58ea9ef529009c4e9efa52507d04
Alexandre Magno <alexandre_pbm@hotmail.com>
2023-02-05 20:49:20 -0300

    Segunda Página Pessoal

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+function validar(){
+    var nome, email, msg;
+    nome = document.getElementById("nome").value;
+    email = document.getElementById("email").value;
+    msg = document.getElementById("msg").value;
+    alert("Nome: "+nome+"\nE=mail: "+email+"\nmsg: "+msg+"\nMensagem enviada!");
+  }
